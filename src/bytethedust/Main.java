250fffb083bd5588705aa075ff67eefe80185b3a
Consti10 <geierconstantinabc@gmail.com>
2020-02-20 21:12:04 +0100

    improve Main

--- src/bytethedust/Main.java
+++ src/bytethedust/Main.java
@@ -7,22 +7,35 @@ public class Main {
 
     public static final String ASSETS_DIRECTORY="C:\\Users\\geier\\Desktop\\HashCode\\HashCode-20-ByteTheDust\\assets\\";
 
+    public static final String[] filenames={
+            "a_example", "b_read_on","c_incunabula","d_tough_choices","e_so_many_books","f_libraries_of_the_world"
+    };
+
     public static void main(String[] args) {
 
         try {
-            final InputDataSet inputDataset = new InputDataSet(ASSETS_DIRECTORY+"a_example.txt");
-            //System.out.println(inputDataset.toString());
 
-            //easiest submission: no lib for scanning
-            final OutputDataSet outputDataSet=inputDataset.createSolution();
+            for(final String filename:filenames){
+                final String path=ASSETS_DIRECTORY+"input\\"+filename+".txt";
+                System.out.println("Processing"+path);
 
-            outputDataSet.writeToFile(ASSETS_DIRECTORY+"solution.txt");
+                final InputDataSet inputDataset = new InputDataSet(path);
+                //System.out.println(inputDataset.toString());
 
-           //final OutputDataSet outputDataSet=Algorithm.generateSimpleStupidSolution(inputDataset);
-           //outputDataSet.writeToFile(ASSETS_DIRECTORY+"solution.txt");
+                //easiest submission: no lib for scanning
+                final OutputDataSet outputDataSet=inputDataset.createSolution();
+
+                outputDataSet.writeToFile(ASSETS_DIRECTORY+"output\\"+filename+"_solution.txt");
+
+            }
+
+
+            //final OutputDataSet outputDataSet=Algorithm.generateSimpleStupidSolution(inputDataset);
+            //outputDataSet.writeToFile(ASSETS_DIRECTORY+"solution.txt");
 
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 }
+
