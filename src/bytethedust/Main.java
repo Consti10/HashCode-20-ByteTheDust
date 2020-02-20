4ee466cd3889539117da9fc6636d11e5eb57c4c1
Consti10 <geierconstantinabc@gmail.com>
2020-02-20 19:36:15 +0100

    new problem

--- src/bytethedust/Main.java
+++ src/bytethedust/Main.java
@@ -7,13 +7,19 @@ public class Main {
 
     //hi consti
 
-    public static final String ASSETS_DIRECTORY="C:\\Users\\lh\\IdeaProjects\\HashCode-20-ByteTheDust\\assets\\";
+    public static final String ASSETS_DIRECTORY="C:\\Users\\geier\\Desktop\\HashCode\\HashCode-20-ByteTheDust\\assets\\";
 
     public static void main(String[] args) {
 
         try {
             final InputDataSet inputDataset = new InputDataSet(ASSETS_DIRECTORY+"a_example.txt");
             //System.out.println(inputDataset.toString());
+
+            //easiest submission: no lib for scanning
+            final OutputDataSet outputDataSet=new OutputDataSet();
+            outputDataSet.N_LIBS=0;
+
+            outputDataSet.writeToFile(ASSETS_DIRECTORY+"solution.txt");
 
            //final OutputDataSet outputDataSet=Algorithm.generateSimpleStupidSolution(inputDataset);
            //outputDataSet.writeToFile(ASSETS_DIRECTORY+"solution.txt");
--- src/bytethedust/OutputDataSet.java
+++ src/bytethedust/OutputDataSet.java
@@ -9,22 +9,33 @@ import java.util.ArrayList;
 
 public class OutputDataSet {
 
-    public final int nTypes;
-    public final ArrayList<Integer> pizzaTypes;
+    public int N_LIBS;
 
-    public OutputDataSet(final int nTypes, final ArrayList<Integer> pizzaTypes){
-        this.nTypes=nTypes;
-        this.pizzaTypes=pizzaTypes;
+    class XXX{
+        public int id;
+        public int N_BOOKS_FOR_SCANNING;
+        public ArrayList<Integer> sendBooks=new ArrayList<>();
     }
+    //first int is id, second int how many books
+    //third to nth int: books
+    public final ArrayList<XXX> xxx=new ArrayList<>();
 
     //Write the output data set into a file
     //Input filename: filename, use absolute path
     public void writeToFile(final String filename){
         StringBuilder builder=new StringBuilder();
-        builder.append(nTypes).append("\n");
-        for(final int val:pizzaTypes){
-           builder.append(val).append(" ");
+        builder.append(N_LIBS).append("\n");
+
+        for(XXX x : xxx){
+            builder.append(x.id).append(" ");
+            builder.append(x.N_BOOKS_FOR_SCANNING).append(" ");
+            builder.append("\n");
+
+            for(int i=0;i<x.sendBooks.size();i++){
+                builder.append(x.sendBooks.get(i)).append(" ");
+            }
         }
+
         try {
             Files.write(Paths.get(filename),builder.toString().getBytes());
         } catch (IOException e) {
